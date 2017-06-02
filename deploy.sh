8ca894dc3653f1b0c871f9b6bd9bc1f728887a19
Alexandre Magno <alexandre.mbm@gmail.com>
2017-06-02 03:57:05 -0300

    deploy.sh refactoring

--- deploy.sh
+++ deploy.sh
@@ -23,62 +23,159 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-echo
-echo "MARKDOWN STATUS"
-echo
-git status
-echo
-echo "HTML STATUS"
-echo
-cd public
-git status
-cd ..
-echo
+if [ "$1" == "install" ]; then
 
-echo -n "   > "
-COMMIT="git commit -a --amend --no-edit";
-if [ -n "$1" ]
-then
-    touch commit.txt
-    echo "$1" > commit.txt
-    COMMIT="git commit -a -F $(pwd)/commit.txt"
-    echo "You will commit with message" 
-else
-    echo "you will amend" 
+    # TODO fixes or cares
+
+    export HUGO_PROJECT=$(pwd)
+    export PATH=$PATH:$HUGO_PROJECT
+    eval $(ssh-agent -s)
+    ssh-add
+    ssh -T git@github.com
+    exit 0
+
 fi
-echo
 
-echo -n "Continue? [y/n] "
-read answer
-if echo "$answer" | grep -iq "^n"
-then
-    exit 1
+if [ -z $HUGO_PROJECT ]; then
+
+    echo "error: hman is not installed"
+    exit 2
+
 fi
-echo
 
-hugo
+function show_help() {
 
-echo
-echo "TARGET"
-echo
-cd public
-git status
-echo
-$COMMIT
-git push -f
-cd ..
-
-echo
-echo "SOURCE"
-echo
-$COMMIT
-echo
-
-if [ -n "$1" ]
-then
-    echo "  > make \"git push\""
     echo
-else
-    echo "  > make \"git push -f\""
+    echo "  hman                     Back to root"
+    echo "       md                  To check Markdown, and can back to root"
+    echo "       html                To check the HTML and enter into it"
+    echo "       commit [message]    Commit"
     echo
-fi
+    echo "       -h | --help         Shows these instructions"
+    echo
+
+}
+
+function untracked_count() {
+    git ls-files --others --exclude-standard | wc -l
+}
+
+function untracked_cd() {
+    [ $(untracked_count) -ne 0 ] && echo -e "\n  > cd $(pwd)"
+}
+
+function untracked_abort() {
+    [ $(untracked_count) -ne 0 ] &&
+        echo -e "\nerror: untracked files \n" &&
+        exit 4
+}
+
+function status_md() {
+
+    cd $HUGO_PROJECT
+    echo
+    echo "MARKDOWN STATUS"
+    echo
+    git status
+    untracked_cd
+    echo
+
+}
+
+function status_html() {
+
+    cd $HUGO_PROJECT
+    echo
+    hugo
+    echo
+    echo "HTML STATUS"
+    echo
+    cd public
+    git status
+    untracked_cd
+    cd ..
+    echo
+
+}
+
+function commit() {
+
+    cd $HUGO_PROJECT
+
+    echo -e -n "\n   > "
+    COMMIT="git commit -a --amend --no-edit";
+    if [ -n "$1" ]
+    then
+        touch commit.txt
+        echo "$1" > commit.txt
+        COMMIT="git commit -a -F $(pwd)/commit.txt"
+        echo "You will commit with message"
+    else
+        echo "You will amend"
+    fi
+    echo
+
+    echo -n "Continue? [y/n] "
+    read answer
+    if echo "$answer" | grep -iq "^n"
+    then
+        exit 3
+    fi
+    echo
+
+    hugo
+    cd public
+    untracked_abort
+    echo
+    echo "HTML STATUS"
+    echo
+    $COMMIT
+    git push -f
+    cd ..
+
+    untracked_abort
+    echo
+    echo "MARKDOWN STATUS"
+    echo
+    $COMMIT
+    echo
+
+    if [ -n "$1" ]
+    then
+        echo "  > make \"git push\""
+        echo
+    else
+        echo "  > make \"git push -f\""
+        echo
+    fi
+
+}
+
+case $1 in
+
+    md)
+        status_md
+        ;;
+
+    html)
+        status_html
+        ;;
+
+    '')
+        echo -e "\n  > cd $HUGO_PROJECT\n"
+        ;;
+
+    -h|--help)
+        show_help
+        ;;
+
+    commit)
+        commit "$2"  # TODO reject $3
+        ;;
+
+    *)
+        echo "error: unknown parameter"
+        exit 1
+        ;;
+
+esac
